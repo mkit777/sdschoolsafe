e6ffa94d7624fb627f25a6ac59a1136a5e6f0439
Joy818 <zy-668@qq.com>
2022-03-15 08:52:24 +0800

    fix bug and add delay

--- auto.py
+++ auto.py
@@ -4,7 +4,7 @@ from extract import do_extract_questions
 from main import load_db, find_answer
 import ddddocr
 import sys
-
+import random
 
 ocr = ddddocr.DdddOcr(show_ad=False)
 
@@ -39,7 +39,7 @@ async def select_school(page, school):
   school_options = page.locator('body > div.el-select-dropdown.el-popper > div.el-scrollbar > div.el-select-dropdown__wrap.el-scrollbar__wrap.el-scrollbar__wrap--hidden-default > ul > li')
   for opt_hd in await school_options.element_handles():
     if (await opt_hd.inner_text()).strip() == school:
-        await opt_hd.click()
+        await opt_hd.click(delay=random.randrange(100, 300))
         break
 
 async def input_username(page, username):
@@ -53,7 +53,7 @@ async def input_password(page, password):
 
 async def refresh_vrcode(page):
   vrc_img = page.locator('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view > uni-view.wrapper > uni-view:nth-child(2) > uni-view:nth-child(3) > uni-image > img')
-  await vrc_img.click()
+  await vrc_img.click(delay=random.randrange(100, 300))
 
 async def input_vrcode(page):
   vrc_img = page.locator('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view > uni-view.wrapper > uni-view:nth-child(2) > uni-view:nth-child(3) > uni-image > img')
@@ -69,7 +69,7 @@ async def input_vrcode(page):
 
 async def login(page):
   confirm_btn = page.locator('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view > uni-view.wrapper > uni-button')
-  await confirm_btn.click()
+  await confirm_btn.click(delay=random.randrange(100, 300))
   resp = await page.wait_for_event('response')
   ret = await resp.json()
   print(f'loing result: {ret}')
@@ -79,27 +79,27 @@ async def login(page):
   
 
 async def start_exam(page):
-  start_btn = page.locator('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(5) > uni-view > uni-view')
-  await start_btn.click()
+  start_btn = page.locator('.kaishi-yuan-class')
+  await start_btn.click(delay=random.randrange(100, 300))
 
 async def conform_start(page):
   confirm_btn = page.locator('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view.uni-popup.center > uni-view:nth-child(2) > uni-view > uni-view > uni-view.paper-foot.margin-top-1.display-flex.flex-justify-content-space-between > uni-button:nth-child(2)')
-  await confirm_btn.click()
+  await confirm_btn.click(delay=random.randrange(100, 300))
 
 
 async def single_select(page, b, no_in_b, opt):
   locator = page.locator(f'body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view.paper-content > .title-contain:nth-child({b + 1}) > .question-contain:nth-child({no_in_b + 2}) > .single-choice uni-label:nth-child({OPT_MAP[opt]})')
-  await locator.click()
+  await locator.click(delay=random.randrange(100, 300))
 
 
 async def multi_select(page, b, no_in_b, opts):
  for opt in opts:
     locator = page.locator(f'body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view.paper-content > .title-contain:nth-child({b + 1}) > .question-contain:nth-child({no_in_b + 2}) > .multiple-choice uni-label:nth-child({OPT_MAP[opt]})')
-    await locator.click()
+    await locator.click(delay=random.randrange(100, 300))
 
 async def judge_select(page, b, no_in_b, opt):
   locator = page.locator(f'body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view.paper-content > .title-contain:nth-child({b + 1}) > .question-contain:nth-child({no_in_b + 2}) > .true-false-choice uni-label:nth-child({OPT_MAP[opt]})')
-  await locator.click()
+  await locator.click(delay=random.randrange(100, 300))
 
 
 async def save_result(page):
@@ -107,13 +107,14 @@ async def save_result(page):
   score = await score_ele.inner_text()
   print(f'分数 {score}')
   qr_code = page.locator('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view.uni-popup.center > uni-view:nth-child(2) > uni-view > uni-view > uni-view:nth-child(2) > uni-image > img')
-  await qr_code.wait_for_element_state('stable')
+  hd = await qr_code.element_handle()
+  await hd.wait_for_element_state('stable')
   await page.screenshot(path='result.png', type='png')
 
 
 async def submit_paper(page):
   locator = page.locator('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view.paper-foot > uni-button')
-  await locator.click()
+  await locator.click(delay=random.randrange(100, 300))
 
 
 async def start_paper(page):
@@ -132,7 +133,7 @@ async def start_paper(page):
     elif q.type == '判断题':
       await judge_select(page, q.block, q.block_no, an[0][0])
 
-    await page.wait_for_timeout(200)
+    await page.wait_for_timeout(random.randrange(300, 500))
 
 
 def init_args():
@@ -157,7 +158,7 @@ def init_args():
 
 async def main():
   async with async_playwright() as p:
-    browser = await p.chromium.launch(headless=False, slow_mo=100)
+    browser = await p.chromium.launch(headless=False)
     page = await init_page(browser)
 
     await page.goto(OPT.url)
@@ -165,14 +166,17 @@ async def main():
     await input_username(page, OPT.username)
     await input_password(page, OPT.password)
     await input_vrcode(page)
-
+    await page.wait_for_timeout(random.randrange(500, 1000))
     success = await login(page)
       
     if success:
       await start_exam(page)
+      await page.wait_for_timeout(random.randrange(500, 1000))
       await conform_start(page)
+      await page.wait_for_timeout(random.randrange(500, 1000))
       await start_paper(page)
       await submit_paper(page)
+      await page.wait_for_timeout(random.randrange(500, 1000))
       await save_result(page)
       
     await browser.close()
